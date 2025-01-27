08264c88e1be2b2a223ee1e7af50667bb59a7c35
mehulsingh <141433508+aashu081@users.noreply.github.com>
2025-01-27 20:24:20 +0000

    datatypes

---
+++ 01_base/02_datatypes.js
@@ -0,0 +1,17 @@
+"user strict";//treat all js code as newer version 
+//alert(3+3) we are using node js, not browser
+console.log(3+3)
+console.log("aashu")
+//neatness of code is also should be preferred
+//code readability should be high
+let name="string"
+let number=123//without commas it is just integer
+//number= 2 to power53
+//bigint
+//string=""
+//boolean => true/false
+//null=>standalone value
+//undefined
+//symbol=> unique
+//object
+console.log(typeof name);
