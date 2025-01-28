8b4dcde287e67d8bdd7ea36bd011f00e15757d9b
mehulsingh <141433508+aashu081@users.noreply.github.com>
2025-01-28 08:22:07 +0000

    conveoperations

---
+++ 01_base/03_conversionoperations.js
@@ -0,0 +1,9 @@
+let score ="33"
+console.log(typeof score)
+console.log(typeof(score))
+let valInNumber= Number(score) //this will make sure it is a number
+console.log(typeof valInNumber)
+let a ="aashu123"
+//ye na to number hai na string hai to yahan par agar isko number me convert karenege to aygea not a  number NaN
+let b=Number(a)
+console.log(typeof b)
