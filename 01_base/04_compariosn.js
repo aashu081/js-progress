b3d3985abd92e56a2accd4543014c43a80037c52
mehulsingh <141433508+aashu081@users.noreply.github.com>
2025-02-02 10:09:42 +0000

    comparison

---
+++ 01_base/04_compariosn.js
@@ -0,0 +1,6 @@
+console.log(null>0)
+console.log(null>=0)
+console.log(null==0)
+console.log("2"==2)
+console.log("2"===2)//this is called strict chechk
+//this compares both data as well as data type not just values
