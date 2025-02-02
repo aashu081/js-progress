14f8542bc57617e5238415df64874100aa8b1e19
mehulsingh <141433508+aashu081@users.noreply.github.com>
2025-02-02 10:45:25 +0000

    summary changes

---
+++ 01_base/05_summary.js
@@ -0,0 +1,15 @@
+// so avaailable data types are of 7 types
+//that is bigint,boolean,string,undefined ,symbol,null ,number (primitive)
+//arrays . objects.functions (refrence type)
+
+
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+//stack(primitive) Heap(non-primitive)
+// if the value is in stack we get the copy of those values not the original one  
+// while in heap the values we get are refrence values
